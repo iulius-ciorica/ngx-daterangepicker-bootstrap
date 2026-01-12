a0f1853781bd48382b6878df2ae9a9f84717fc13
iulius.ciorica <iulius.ciorica@adora.tech>
2026-01-12 13:23:10 +0200

    angular 21 upgrade

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
+    await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngx-daterangepicker-bootstrap-sdk-a20');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngx-daterangepicker-bootstrap-sdk-a21');
   });
 });
